8851b2a1752d1b41ec5ecf2925a3db76e207639d
James Tippett <james.tippett@gmail.com>
2024-06-01 14:47:39 +0700

    updates for ethers v6 & mainnet deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+const { API_URL, MAIN_API_URL, PRIVATE_KEY } = process.env;
+
+console.log ("process.env: ", process.env);
+
 module.exports = {
   solidity: "0.8.24",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: MAIN_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,19 @@
+const { ethers } = require("hardhat");
+
 async function main() {
   const [deployer] = await ethers.getSigners();
-
   console.log("Deploying contracts with the account:", deployer.address);
+  const accountBalance = await deployer.provider.getBalance(deployer.address);
 
-  const weiAmount = (await deployer.getBalance()).toString();
+  // const weiAmount = (await deployer.getBalance()).toString();
   
-  console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+  console.log("Account balance:", ethers.formatEther(accountBalance));
+  const initialOwner = deployer.address;
 
   const Token = await ethers.getContractFactory("Veratix");
-  const token = await Token.deploy();
+  const token = await Token.deploy(initialOwner);
 
-  console.log("Token address:", token.address);
+  console.log("Token address:", token.target); // In ethers v6, use token.target instead of token.address
 }
 
 main()
